02944cf95f6602984f26b2ac41ac9e302e90aa25
张华斌 <823506006@qq.com>
2016-08-24 16:18:35 +0800

    init

---
+++ src/com/nerin/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.nerin.demo;
+
+/**
+ * Created by 张华斌 on 2016/8/24.
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hello world...");
+    }
+}
